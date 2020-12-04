626517c38bf365c7f693a129d24fe39b29388e6f
John Baylor <john.baylor@gmail.com>
2020-12-04 15:14:41 -0800

    Solve D01P2 and D02P1.

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,9 +1,14 @@
 defmodule AdventOfCode.Day01 do
-  def part1(args) do
-    first_number =
-        args
-        |> Enum.find(fn(number) -> Enum.member?(args, 2020 - number) end)
+  def match_target?(target, whole_list) do
+    result =
+      whole_list
+      |> Enum.find(fn(number) -> Enum.member?(whole_list, target - number) end)
 
+    result
+  end
+
+  def part1(args) do
+    first_number = match_target?(2020, args)
     second_number = 2020 - first_number
     result = first_number * second_number
     IO.puts("#{first_number} + #{second_number} == #{result}")
@@ -11,6 +16,26 @@ defmodule AdventOfCode.Day01 do
     result
   end
 
-  def part2(_args) do
+  def get_three_numbers(first_number, args) do
+    target = 2020 - first_number
+    second_number = match_target?(target, args)
+    case second_number do
+      nil -> []
+      x -> [first_number, second_number, 2020 - first_number - second_number]
+    end
+  end
+
+  def part2(args) do
+    [a, b, c] = Enum.reduce(args, [], fn(number, acc) ->
+      all_three = get_three_numbers(number, args)
+      case {acc, all_three} do
+        {[], []} -> []
+        {[], [aa, bb, cc]} -> [aa, bb, cc]
+        {x, _y} -> x
+      end
+    end)
+    IO.inspect("#{a} + #{b} + #{c}")
+
+    a * b * c
   end
 end
--- lib/advent_of_code/day_02.ex
+++ lib/advent_of_code/day_02.ex
@@ -1,5 +1,28 @@
 defmodule AdventOfCode.Day02 do
-  def part1(_args) do
+  def valid_line?(line) do
+    [range, char, password] = String.split(line, " ")
+    [low, high] = String.split(range, "-")
+    char = String.slice(char, 0, 1)
+#    IO.puts("Are there #{low} to #{high} letter '#{char}' in '#{password}'?")
+    {:ok, regex} = Regex.compile(char)
+    matches = Regex.scan(regex, password) |> Enum.count()
+    low = String.to_integer(low)
+    high = String.to_integer(high)
+#    IO.inspect([matches, low, high, regex])
+
+    (low <= matches && high >= matches)
+  end
+
+  def part1(string) do
+    string
+    |> String.trim
+    |> String.split("\n")
+    |> Enum.reduce(0, fn(line, acc) ->
+      case valid_line?(line) do
+        true -> acc + 1
+        _ -> acc
+      end
+    end)
   end
 
   def part2(_args) do

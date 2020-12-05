d36c636a70f09f6929d6b761c72118bd803d7c51
John Baylor <john.baylor@gmail.com>
2020-12-05 11:33:35 -0800

    D04P1

--- lib/advent_of_code/day_04.ex
+++ lib/advent_of_code/day_04.ex
@@ -1,5 +1,32 @@
 defmodule AdventOfCode.Day04 do
-  def part1(_args) do
+  def has_p1_keys?(map) do
+    keys = Map.keys(map) |> Enum.sort
+    # IO.inspect(keys)
+
+    keys == ~w[byr cid ecl eyr hcl hgt iyr pid] ||
+    keys == ~w[byr     ecl eyr hcl hgt iyr pid]
+  end
+
+  def part1(args) do
+    result = args
+             |> String.split("\n\n")
+             |> Enum.reduce(%{valid_passports: 0}, fn(chunk, %{valid_passports: valid_passports}) ->
+      data =
+        String.split(chunk, ~r/\s+/m)
+        |> Enum.map(fn(field) ->
+          [key, val] = String.split(field, ":")
+          {key, val}
+        end)
+        |> Map.new
+
+      case has_p1_keys?(data) do
+        true -> %{valid_passports: valid_passports + 1}
+        _ -> %{valid_passports: valid_passports}
+
+      end
+    end)
+
+    result.valid_passports
   end
 
   def part2(_args) do
--- lib/mix/tasks/d04.p1.ex
+++ lib/mix/tasks/d04.p1.ex
@@ -5,7 +5,8 @@ defmodule Mix.Tasks.D04.P1 do
 
   @shortdoc "Day 04 Part 1"
   def run(args) do
-    input = nil
+#    input = File.read!('/Users/johnb/dev/2020adventOfCode/lib/mix/tasks/d04.example.txt')
+    input = File.read!('/Users/johnb/dev/2020adventOfCode/lib/mix/tasks/d04.input.txt')
 
     if Enum.member?(args, "-b"),
       do: Benchee.run(%{part_1: fn -> input |> part1() end}),

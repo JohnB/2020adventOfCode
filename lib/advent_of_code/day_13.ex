d7da1f3927051f766c10448ae4a8ebb373208a17
John Baylor <john.baylor@gmail.com>
2020-12-13 23:25:21 -0800

    D14: Ugly mess

--- lib/advent_of_code/day_13.ex
+++ lib/advent_of_code/day_13.ex
@@ -3,9 +3,133 @@ defmodule AdventOfCode.Day13 do
 
   def part1(args) do
     result = as_single_lines(args)
+    earliest = List.first(result) |> String.to_integer
+               |> IO.inspect
+    buses =
+      List.last(result)
+      |> String.split(",")
+      |> Enum.filter(fn(value) -> value != "x" end)
+      |> Enum.map(fn(value) -> String.to_integer(value) end)
+      |> IO.inspect
+
+    # What multiple of bus number is the least past "earliest"?
+    multiples = Enum.map(buses, fn(bus) -> {bus, (Float.floor(earliest / bus) + 1) * bus} end)
+                |> IO.inspect
+
+    {bus, arrival} =
+          Enum.sort_by(multiples, fn({bus, arrival}) -> arrival end )
+                                                        |> List.first
+                                                        |> IO.inspect
+
+    bus * (arrival - earliest)
   end
 
   def part2(args) do
     result = as_single_lines(args)
+    #    earliest = List.first(result) |> String.to_integer
+
+    buses =
+      List.last(result)
+      |> String.split(",")
+      |> Enum.with_index
+      #|> IO.inspect
+      |> Enum.map(fn({bus, index}) ->
+        case bus do
+          "x" -> {nil, nil}
+          _ -> {String.to_integer(bus), index}
+        end
+      end)
+      |> Enum.filter(fn({bus, index}) -> bus != nil end)
+      |> Enum.sort_by(fn({bus, arrival}) -> arrival end )
+      |> IO.inspect
+
+      # example
+      #    [{7, 0}, {13, 1}, {59, 4}, {31, 6}, {19, 7}]
+      #
+      #     Enum.sort
+      #   [{7, 0}, {13, 1}, {19, 7}, {31, 6}, {59, 4}]
+
+# real input
+#      by_arrival =
+#      [
+#        {19, 0},
+#        {41, 9},
+#        {643, 19}, #
+#        {17, 36},  # 4 x 9
+#        {13, 37},
+#        {23, 42},  # 2 * 3 * 7
+#        {509, 50},
+#        {37, 56},
+#        {29, 79}
+#      ]
+      start = 19 * (643 - 1)
+      increments = 643
+
+
+    #    foo = [
+#    foo = [
+    #  {13, 37},
+    #  {17, 36},
+    #  {19, 0},
+    #  {23, 42},
+    #  {29, 79},
+    #  {37, 56},
+    #  {41, 9},
+    #  {509, 50},
+    #  {643, 19}
+    # all the buses are prime, so the time between timestamps is
+    # the product of them all
+    # and the distance "back" to t is
+    # each of the arrival offsets by all the *other* primes
+    #]
+#      {19, 0},
+#      {41, 9},
+#      {643, 19},
+#      {17, 36},
+#      {13, 37},
+#      {23, 42},
+#      {509, 50},
+#      {37, 56},
+#      {29, 79}
+#    ]
+
+
+    {first_bus, _} = List.first(buses)
+                     |> IO.inspect
+#    find_result(first_bus, buses, 1730426330)
+    #find_result(first_bus, buses, 99999999999984)
+  end
+
+
+  def is_multiple?(big, small) do
+    multiple = big / small
+    {x, y} = Float.ratio(multiple)
+    y == 1
+  end
+
+  def find_result(first_bus, buses, start) do
+    IO.puts(start)
+    success = Enum.all?(buses, fn({bus, offset}) ->
+      is_multiple?(start + offset, bus)
+    end)
+    case success do
+      true -> start
+      _ -> find_result(first_bus, buses, start + first_bus)
+    end
   end
 end
+
+"""
+[
+  {19, 0},
+  {41, 9},
+  {643, 19}, # allows us to multiply?
+  {17, 36},
+  {13, 37},
+  {23, 42},
+  {509, 50},
+  {37, 56},
+  {29, 79}
+]
+
+"""
--- lib/advent_of_code/day_14.ex
+++ lib/advent_of_code/day_14.ex
@@ -1,11 +1,136 @@
 defmodule AdventOfCode.Day14 do
   import AdventOfCode
 
+  @zeros [
+    "0", "0", "0", "0", "0", "0", "0", "0", "0",
+    "0", "0", "0", "0", "0", "0", "0", "0", "0",
+    "0", "0", "0", "0", "0", "0", "0", "0", "0",
+    "0", "0", "0", "0", "0", "0", "0", "0", "0",
+  ]
+
   def part1(args) do
-    result = as_single_lines(args)
+    start = %{mask: []}
+    result =
+      as_single_lines(args)
+      |> Enum.reduce(start, fn(line, acc) ->
+        words = String.split(line, " = ", trim: true)
+        case List.first(words) do
+          "mask" -> %{acc | mask: List.last(words) |> String.split("", trim: true) }
+          _ -> apply1(acc, words)
+        end
+      end)
+      |> Enum.reduce(0, fn({k, v}, acc) ->
+        case k do
+          :mask -> acc
+          _ -> acc + String.to_integer(v, 2)
+        end
+      end)
   end
 
   def part2(args) do
-    result = as_single_lines(args)
+    start = %{mask: []}
+    result =
+      as_single_lines(args)
+      |> Enum.reduce(start, fn(line, acc) ->
+        words = String.split(line, " = ", trim: true)
+        case List.first(words) do
+          "mask" -> %{acc | mask: List.last(words) |> String.split("", trim: true) }
+          _ -> apply2(acc, words)
+        end
+      end)
+      |> IO.inspect(label: "ready?")
+      |> Enum.reduce(0, fn({k, v}, acc) ->
+        IO.inspect([k, v, acc])
+        case k do
+          :mask -> acc
+          _ -> acc + v #String.to_integer(v, 2)
+        end
+      end)
+  end
+
+  def apply2(acc = %{mask: mask}, [store_cmd, value]) do
+    # "mem[8]" means that 8 is at position 4
+    {address, _junk} = String.slice(store_cmd, 4..-1) |> Integer.parse()
+    binary =
+      (@zeros ++ Integer.digits(address, 2))
+      |> Enum.take(-36)
+
+    updated_memory =
+      find_addrs(mask, binary, "")
+      |> IO.inspect
+      |> Enum.reduce(%{}, fn(addr, map) ->
+        Map.put(map, addr, String.to_integer(value))
+      end )
+
+      Map.merge(acc, updated_memory)
+  end
+
+  def enumerate_addrs([], result) do
+#    IO.inspect(result, label: "enumerate_addrs hit bottom")
+
+    addr =
+      result
+      |> Enum.join
+      |> String.to_integer(2)
+      |> IO.inspect(label: "bottom")
+
+    [addr]
+  end
+
+  def enumerate_addrs([addr0 | addr_rest], so_far) do
+#    IO.inspect([addr0, addr_rest, so_far])
+    case addr0 do
+      "X" -> enumerate_addrs(["1" | addr_rest], so_far) ++ enumerate_addrs(["0" | addr_rest], so_far)
+      _ -> enumerate_addrs(addr_rest, so_far ++ [addr0])
+    end
+  end
+
+  def find_addrs([], [], acc ) do
+#    IO.inspect(acc, label: "should have floating bits")
+    enumerate_addrs(String.split(acc, "", trim: true), [])
+#    |> IO.inspect(label: "end of find_addrs")
+  end
+
+  def find_addrs([mask0 | mask_rest], [bit | other_bits], acc ) do
+    case mask0 do
+      "0" -> find_addrs(mask_rest, other_bits, acc <> "#{bit}")
+      "1" -> find_addrs(mask_rest, other_bits, acc <> "1")
+      "X" -> find_addrs(mask_rest, other_bits, acc <> "X")
+    end
+  end
+
+  def masked_values(mask, value) do
+    binary =
+      (@zeros ++ Integer.digits(value, 2))
+      |> Enum.take(-36)
+
+    apply_mask(mask, binary, [])
+    |> Enum.join
+  end
+
+  ### part1 functions
+
+  def apply1(acc = %{mask: mask}, [store_cmd, value]) do
+    # "mem[8]" means that 8 is at position 4
+    {address, _junk} = String.slice(store_cmd, 4..-1) |> Integer.parse()
+    Map.put(acc, address, masked_value(mask, String.to_integer(value)))
+    |> IO.inspect
+  end
+
+  def masked_value(mask, value) do
+    binary =
+      (@zeros ++ Integer.digits(value, 2))
+      |> Enum.take(-36)
+
+    apply_mask(mask, binary, [])
+    |> Enum.join
+  end
+
+  def apply_mask([], [], acc ) do acc end
+  def apply_mask([mask0 | mask_rest], [bit | other_bits], acc ) do
+    case mask0 do
+      "X" -> apply_mask(mask_rest, other_bits, acc ++ [bit])
+      _ -> apply_mask(mask_rest, other_bits, acc ++ [mask0])
+    end
   end
 end
